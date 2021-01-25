6491f3125474166ea90136cba1f98f30b3c220bf
Janakaraj <janak@promactinfo.com>
2021-01-25 19:03:47 +0530

    Add project files.

---
+++ ci-cd-dotnet-core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ci_cd_dotnet_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
