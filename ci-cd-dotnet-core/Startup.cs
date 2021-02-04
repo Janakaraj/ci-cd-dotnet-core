a4d3561a9532a5e2651317431f7d3dacbd0aca82
Janakaraj <janak@promactinfo.com>
2021-02-04 20:31:28 +0530

    Add httpsport

--- ci-cd-dotnet-core/Startup.cs
+++ ci-cd-dotnet-core/Startup.cs
@@ -24,6 +24,7 @@ namespace ci_cd_dotnet_core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpsRedirection(opt => opt.HttpsPort = 443);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
